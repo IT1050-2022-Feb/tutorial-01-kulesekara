34d84fe4867f719c6e29dd5aef3b504c98f79bbe
IT20261382 <105878356+kulesekara@users.noreply.github.com>
2022-06-14 06:11:39 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,30 @@
-/* Exercise 1 - Calculations
+#include<stdio.h>
 
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+int main(){
+  int maths, physics, marks,average;
+  printf("Please input the marks of maths : ");
+  scanf("%d", &maths);
 
-#include <stdio.h>
-
-int main() {
   
-  return 0;
-}
+    if (maths<=100){
+  printf("Please input the marks of physics : ");
+  scanf("%d", &physics);
+    }
 
+
+ if (physics<=100){
+
+  marks=maths+physics;
+  average=marks/2;
+
+
+  printf("Total marks are -->%d",marks);
+  printf("\n");
+  printf("average is -->%d",average);
+    }
+  
+  else 
+    printf("Invalide marks");
+    
+    return 0;
+}
